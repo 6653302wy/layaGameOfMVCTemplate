dff9e844f99434fc162be1412af1d96ad4f5e2c0
wanpp <wanpingping@innotechx.com>
2020-03-17 14:17:52 +0800

    feat: add png compress tool

--- tiny.js
+++ tiny.js
@@ -1,24 +1,30 @@
-const gulp = require('gulp');
-// const webp = require('gulp-webp');
+const gulp = require("gulp");
+const webp = require("gulp-webp");
 
-// gulp.task('tiny', () => {
-//     const imageRoot = '../bin/res/exchange/';
-//     return gulp.src(`${imageRoot}/**/*.{png,jpg,jpeg}`)
-//         .pipe(webp())
-//         .pipe(gulp.dest(`../bin/webp/`));
-// });
+const resPath = "./bin/res/**/*.{png,jpg,jpeg}";
+const testRes = "bin/res/GUI";
 
-// gulp.task('default', gulp.series('tiny'));
-
-
-const imageisux = require("gulp-imageisux");
-
-gulp.task("imageisux", function () {
-    return gulp
-        .src(["bin/res/**/*.{png,jpg,jpeg}"])
-        .pipe(imageisux("", true));
+gulp.task('tiny', () => {
+    return gulp.src(resPath)
+        .pipe(webp())
+        .pipe(gulp.dest(`./bin/webp/`));
 });
-gulp.task("default", gulp.series("imageisux"));
+gulp.task('default', gulp.series('tiny'));
+
+// 压缩图片
+// const imagemin = require('gulp-imagemin');
+// exports.default = () => (
+//     gulp.src(respath)
+//     .pipe(imagemin())
+//     .pipe(gulp.dest('../bin/res/tiny/'))
+// );
+
+// 智图压图兼转webp
+// const imageisux = require("gulp-imageisux");
+// gulp.task("imageisux", function() {
+//   return gulp.src([resPath]).pipe(imageisux("", true));
+// });
+// gulp.task("default", gulp.series("imageisux"));
 
 // 监听文件夹变化
 // gulp.task('watch', () =>
@@ -27,3 +33,19 @@ gulp.task("default", gulp.series("imageisux"));
 // gulp.task('default', () =>
 //     gulp.start('watch')
 // );
+
+// tinypng
+// var tinypng = require("gulp-tinypng-compress");
+// gulp.task("tinypng", function () {
+//     gulp
+//         .src(resPath)
+//         .pipe(
+//             tinypng({
+//                 key: "TQ9x5ZYkDhRpNcyRlHcQnTm2dY6R8WjN",
+//                 sigFile: "images/.tinypng-sigs",
+//                 log: true
+//             })
+//         )
+//         .pipe(gulp.dest("./bin/res/tinyimages"));
+// });
+// gulp.task("default", gulp.series("tinypng"));
